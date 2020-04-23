ac6a45d87c8c1f40c9e896ceeb828fc29e6cd455
sekaiiiii <qq738359456@gmail.com>
2020-04-24 03:40:31 +0800

    complete basic structure of index

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,23 @@
+//引入组件
 import Vue from 'vue'
 import Index from '../components/Index.vue'
 import Login from '../components/Login.vue'
+import PageNotFound from "../components/PageNotFound"
+import Test1 from "../components/Test1.vue"
+import Test2 from "../components/Test2.vue"
+import Test3 from "../components/Test3.vue"
+
 
 import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path:'/',
+    name:"root",
+    redirect:"/login"
+  },
   {
     path: '/login',
     name: 'login',
@@ -15,7 +26,26 @@ const routes = [
   {
     path: '/index',
     name: 'index',
-    component : Index
+    component : Index,
+    children:[
+      {
+        path:"1",
+        component:Test1
+      },
+      {
+        path:"2",
+        component:Test2
+      },
+      {
+        path:"3",
+        component:Test3
+      }
+    ]
+  },
+  {
+    path: '*',
+    name: '404',
+    component: PageNotFound
   }
 ]
 
